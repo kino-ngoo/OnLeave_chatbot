6041037486f2437eb55406660653c037d4f584a1
57 <mrrfvg@gmail.com>
2018-11-26 12:04:59 +0800

    Get LUIS

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+const express = require('express');
 const http = require('http');
 const restify = require('restify');
 const builder = require('botbuilder');
+const botbuilder_azure = require('botbuilder-azure');
+
+require('dotenv-extended').load();
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -10,8 +14,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword
+    appId: 'c150192f-48d3-4bc5-82b0-1f0f9abb258f',
+    appPassword: 'warTCL96+)ejurFGVP904|)'
 });
 
 // Listen for messages from users 
@@ -28,3 +32,49 @@ var bot = new builder.UniversalBot(connector, function (session) {
 //   res.end('Hello World\n');
 // }).listen(1337, '127.0.0.1');
 // console.log('Server running at http://127.0.0.1:1337/');
+
+require('dotenv').config();
+
+var request = require('request');
+var querystring = require('querystring');
+
+function getLuisIntent(utterance) {
+    var endpoint =
+        "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/";
+
+    // Set the LUIS_APP_ID environment variable 
+    // to df67dcdb-c37d-46af-88e1-8b97951ca1c2, which is the ID
+    // of a public sample application.    
+    var luisAppId = '!!!!!!!換成你的AppId!!!!!!!';
+
+    // Set the LUIS_SUBSCRIPTION_KEY environment variable
+    // to the value of your Cognitive Services subscription key
+    var queryParams = {
+        "subscription-key": '!!!!!!!換成你的Key!!!!!!!',
+        "timezoneOffset": "0",
+        "verbose":  true,
+        "q": utterance
+    }
+
+    var luisRequest =
+        endpoint + luisAppId +
+        '?' + querystring.stringify(queryParams);
+
+    request(luisRequest,
+        function (err,
+            response, body) {
+            if (err)
+                console.log(err);
+            else {
+                var data = JSON.parse(body);
+
+                console.log(`Query: ${data.query}`);
+                console.log(`Top Intent: ${data.topScoringIntent.intent}`);
+                console.log('Intents:');
+                console.log(data.intents);
+            }
+        });
+}
+
+// Pass an utterance to the sample LUIS app
+getLuisIntent('turn on the left light');
